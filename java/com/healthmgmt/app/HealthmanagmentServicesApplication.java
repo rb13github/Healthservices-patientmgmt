aa76567efc39120fb57c1a0ea3916fac5ac4e2c0
rb13github <rbhondwe@gmail.com>
2020-07-16 00:41:15 +0530

    newUI
    to use environment variable and print them in main class

--- java/com/healthmgmt/app/HealthmanagmentServicesApplication.java
+++ java/com/healthmgmt/app/HealthmanagmentServicesApplication.java
@@ -1,5 +1,11 @@
 package com.healthmgmt.app;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +13,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableSwagger2WebMvc
 public class HealthmanagmentServicesApplication {
 
+	 private static final Logger logger = LoggerFactory.getLogger(HealthmanagmentServicesApplication.class);
+	 @Autowired
+	    private static Environment env;
 	public static void main(String[] args) {
+		
+		String dburl = System.getenv().getOrDefault("DB_URL", "Environment variable not set");
+		System.out.println("DB URL : "+dburl); 
+		
+		logger.info("{}", System.getenv("DB_URL"));
+	       
+		
+		Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+        	
+        	logger.trace("{}", envName+"="+env.get(envName));
+        	
+        }
+        
+		
+		//System.out.println("DB URL env java home: "+env.getProperty("JAVA_HOME")); 
+		
+		
+		
 		SpringApplication.run(HealthmanagmentServicesApplication.class, args);
 	}
 
